c7ae79ecdc4597bac4397357810b279f2e4f0c8a
marincug <marincug@gmail.com>
2017-09-28 16:37:58 +0100

    Thursday 28/09 workEnd

--- exerciseJavaScripting.js
+++ exerciseJavaScripting.js
@@ -7,6 +7,8 @@ window.onload = function () {
 }
 
 let x;
+let garageArray = [];
+let listNumber = 0;
 let person = {
     name: 'Gratiani',
     age: '24',
@@ -64,13 +66,13 @@ function ConcatenateButton() {
 }
 
 function AddButton() {
-    ray += "," + " " + document.getElementById('txta1').value;
+    ray.push(document.getElementById('txta1').value);
     document.getElementById('DisplayArray').innerHTML = ray;
 }
 
 function RemoveButton() {
-    ray.pop();
     document.getElementById('DisplayArray').innerHTML = ray;
+    ray.pop();
 }
 
 function FizzBuzz() {
@@ -119,8 +121,6 @@ function Divisibility() {
 
 
 function CheckForTripple() {
-
-
     x = document.getElementById('TrippleCheck').value
     y = 0;
     for (let i = 3; i < x.length; i++) {
@@ -137,7 +137,6 @@ function CreateParagraphs() {
     p2.innerHTML = "This is Paragraph 2"
     p2.setAttribute("id", "p2")
     document.getElementById('ParagraphManipulation').appendChild(p2)
-
 }
 
 function CreateParagraphsFromText() {
@@ -146,7 +145,6 @@ function CreateParagraphsFromText() {
     p3.setAttribute("id", "p3")
     document.getElementById('ParagraphManipulation').appendChild(p3)
 }
-
 
 function RemoveParagraphs() {
     document.getElementById('ParagraphManipulation').removeChild(document.getElementById('p2'))
@@ -160,7 +158,6 @@ function JSON() {
     request.send();
     request.onload = function () {
         let requestData = request.response;
-
         let myP1 = document.createElement(`p`);
         let myP2 = document.createElement(`p`);
         let myP3 = document.createElement(`p`);
@@ -181,14 +178,11 @@ function JSON() {
             myH1.textContent += "   Age: ";
             myH1.textContent += "\n" + requestData[`members`][i].age + " ----- ";
             myH1.textContent += "  Real Identity: ";
-            myH1.textContent += "\n" + requestData[`members`][i].secretIdentity + " ----- "
-            ;
+            myH1.textContent += "\n" + requestData[`members`][i].secretIdentity + " ----- ";
             myH1.textContent += "  Powers:  ";
             for (let j = 0; j < requestData["members"][i]["powers"].length; j++) {
                 myH1.textContent += "\n" + requestData[`members`][i][`powers`][j] + ", ";
-
             }
-
             document.getElementById('json6').appendChild(myH1);
         }
         document.getElementById('json1').appendChild(myP1)
@@ -196,51 +190,100 @@ function JSON() {
         document.getElementById('json3').appendChild(myP3)
         document.getElementById('json4').appendChild(myP4)
         document.getElementById('json5').appendChild(myP5)
-
-
     }
 }
-    function JSON2() {
-        let requestURL2 = `https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json`;
-        let request2 = new XMLHttpRequest();
-        request2.open(`GET`, requestURL2);
-        request2.responseType = `json`
-        request2.send();
-        request2.onload = function () {
-            let requestData2 = request2.response
 
-            let x = document.getElementById('SearchJSON2').value;
+function JSON2() {
+    let requestURL2 = `https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json`;
+    let request2 = new XMLHttpRequest();
+    request2.open(`GET`, requestURL2);
+    request2.responseType = `json`
+    request2.send();
+    request2.onload = function () {
+        let requestData2 = request2.response
 
-            for (let i = 0; i<requestData2.length; i++){
-                let myH1 = document.createElement(`p`);
-                if (requestData2[i].nm.includes(x)){
-                    myH1.textContent += requestData2[i].nm + ", from: ";
-                    myH1.textContent += requestData2[i].cty + ", of: ";
-                    myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
-                    myH1.textContent += requestData2[i].yrs;
-                }
-                if (requestData2[i].cty.includes(x)){
-                    myH1.textContent += requestData2[i].nm + ", from: ";
-                    myH1.textContent += requestData2[i].cty + ", of: ";
-                    myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
-                    myH1.textContent += requestData2[i].yrs;
-                }
-                if (requestData2[i].hse.includes(x)){
-                    myH1.textContent += requestData2[i].nm + ", from: ";
-                    myH1.textContent += requestData2[i].cty + ", of: ";
-                    myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
-                    myH1.textContent += requestData2[i].yrs;
-                }
-                if (requestData2[i].yrs.includes(x)){
-                    myH1.textContent += requestData2[i].nm + ", from: ";
-                    myH1.textContent += requestData2[i].cty + ", of: ";
-                    myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
-                    myH1.textContent += requestData2[i].yrs;
-                }
-                document.getElementById('Sjson').appendChild(myH1)
+        let x = document.getElementById('SearchJSON2').value;
+
+        for (let i = 0; i < requestData2.length; i++) {
+            let myH1 = document.createElement(`p`);
+            if (requestData2[i].nm.includes(x)) {
+                myH1.textContent += requestData2[i].nm + ", from: ";
+                myH1.textContent += requestData2[i].cty + ", of: ";
+                myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
+                myH1.textContent += requestData2[i].yrs;
             }
-
+            if (requestData2[i].cty.includes(x)) {
+                myH1.textContent += requestData2[i].nm + ", from: ";
+                myH1.textContent += requestData2[i].cty + ", of: ";
+                myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
+                myH1.textContent += requestData2[i].yrs;
+            }
+            if (requestData2[i].hse.includes(x)) {
+                myH1.textContent += requestData2[i].nm + ", from: ";
+                myH1.textContent += requestData2[i].cty + ", of: ";
+                myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
+                myH1.textContent += requestData2[i].yrs;
+            }
+            if (requestData2[i].yrs.includes(x)) {
+                myH1.textContent += requestData2[i].nm + ", from: ";
+                myH1.textContent += requestData2[i].cty + ", of: ";
+                myH1.textContent += requestData2[i].hse + ", lived between the years of: ";
+                myH1.textContent += requestData2[i].yrs;
+            }
+            document.getElementById('Sjson').appendChild(myH1);
         }
-
     }
+}
+
+
+function AddToGarrageButton() {
+
+    let oName = document.getElementById('CarOwnerName').value;
+    let oSurname = document.getElementById('CarOwnerSurname').value;
+    let cMake = document.getElementById('CarMake').value;
+    let cEngine = document.getElementById('CarEngine').value;
+    let cWheels = document.getElementById('CarWheels').value;
+    let temporaryFaults = document.getElementById('CarFaultList')
+    let cFaults = temporaryFaults.options[temporaryFaults.selectedIndex].value
+    let repairCost = 0;
+    let x = { oName, oSurname, cMake, cEngine, cWheels };
+    garageArray.push(x);
+
+    let displayList = document.createElement(`p`)
+    let totalCost = document.createElement(`p`)
+
+    listNumber = garageArray.length
+    displayList.textContent = listNumber + ") Owner Name: " + garageArray[garageArray.length - 1].oName + " ----" + " Owner Surname: " + oSurname + " ----" + " Car Make: " + cMake + " ---- " + cFaults;
+    displayList.setAttribute("id", "DL" + (garageArray.length - 1));
+    document.getElementById('CarList').appendChild(displayList);
+
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "No Faults, MOT") repairCost = 1;
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Faulty Tires") repairCost = 1;
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Faulty Lights") repairCost = 2;
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Faulty Dashboard") repairCost = 3;
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Faulty Electrics") repairCost = 4;
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Faulty Engine") repairCost = 5;
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Unknown Faulty") repairCost = "To be determined on arrival";
+    if (temporaryFaults.options[temporaryFaults.selectedIndex].value === "Multiple Faults"){
+     
+    }
+
+    totalCost.textContent = "Total cost: " + repairCost + "£";
+    totalCost.setAttribute("id", "RCL" + (garageArray.length - 1));
+    document.getElementById('CarList').appendChild(totalCost)
+
+}
+
+function RemoveFromGarageButton() {
+    let x = document.getElementById('NameToRemove').value;
+    let y = document.getElementById('SurnameToRemove').value;
+    for (let i = 0; i < garageArray.length; i++) {
+        if (garageArray[i].oName.includes(x) && garageArray[i].oSurname.includes(y)) {
+            document.getElementById('CarList').removeChild(document.getElementById('DL' + i));
+            document.getElementById('CarList').removeChild(document.getElementById('RCL' + i));
+            garageArray.splice(i, 1);
+            listNumber = garageArray.length;
+        }
+    }
+}
 
